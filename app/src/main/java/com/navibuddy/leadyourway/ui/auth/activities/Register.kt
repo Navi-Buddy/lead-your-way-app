22a954678d699997195d53da6f110946cfcb6fca
JosR <josueramos17@hotmail.com>
2023-09-27 11:09:22 -0500

    refactor: create RepeatedPasswordTextField component

--- app/src/main/java/com/navibuddy/leadyourway/ui/auth/activities/Register.kt
+++ app/src/main/java/com/navibuddy/leadyourway/ui/auth/activities/Register.kt
@@ -7,37 +7,25 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.Visibility
-import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.FilledIconButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.navibuddy.leadyourway.R
 import com.navibuddy.leadyourway.ui.auth.components.EmailTextField
 import com.navibuddy.leadyourway.ui.auth.components.PasswordTextField
+import com.navibuddy.leadyourway.ui.auth.components.RepeatedPasswordTextField
 import com.navibuddy.leadyourway.ui.navigation.Routes
 
 @Composable
@@ -46,7 +34,6 @@ fun Register(navController: NavController) {
     val (password, setPassword) = remember { mutableStateOf("") }
     val (showPassword, setShowPassword) = remember { mutableStateOf(false) }
     val (repeatedPassword, setRepeatedPassword) = remember { mutableStateOf("") }
-    var showRepeatedPassword by remember { mutableStateOf(false) }
 
     Column(
         verticalArrangement = Arrangement.SpaceAround,
@@ -81,24 +68,9 @@ fun Register(navController: NavController) {
                 setShowPassword = setShowPassword
             )
             Spacer(Modifier.padding(8.dp))
-            OutlinedTextField(
-                modifier = Modifier.fillMaxWidth(),
-                value = repeatedPassword,
-                onValueChange = setRepeatedPassword,
-                leadingIcon = { Icon(Icons.Filled.Lock, "Lock") },
-                label = { Text(text = "Confirm password") },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                visualTransformation =
-                if (showRepeatedPassword) VisualTransformation.None else PasswordVisualTransformation(),
-                trailingIcon = {
-                    IconButton(onClick = { showRepeatedPassword = !showRepeatedPassword }) {
-                        if (showRepeatedPassword)
-                            Icon(Icons.Filled.Visibility, "Visibility on")
-                        else
-                            Icon(Icons.Filled.VisibilityOff, "Visibility off")
-
-                    }
-                }
+            RepeatedPasswordTextField(
+                repeatedPassword = repeatedPassword,
+                setRepeatedPassword = setRepeatedPassword
             )
             Spacer(Modifier.padding(3.dp))
             if (password != repeatedPassword) {

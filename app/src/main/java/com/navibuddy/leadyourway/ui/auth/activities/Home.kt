b2593f4a356d0f2d56068d7eee76a0d735fa3b86
oliverTuesta <oliver.jtuesta@gmail.com>
2023-09-27 19:21:32 -0500

    feat: add map image to home

--- app/src/main/java/com/navibuddy/leadyourway/ui/auth/activities/Home.kt
+++ app/src/main/java/com/navibuddy/leadyourway/ui/auth/activities/Home.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.navibuddy.leadyourway.ui.auth.components.BottomNavigation
+import com.skydoves.landscapist.glide.GlideImage
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Preview
@@ -19,5 +20,9 @@ fun Home() {
             Text(text = "Home", modifier = Modifier.padding(16.dp))
         },
         bottomBar = { BottomNavigation() }
+    )
+    GlideImage(
+        imageModel = { "https://www.nalc.gov.uk/images/easyblog_articles/1110/bigstock--204040621.jpg" },
+        modifier = Modifier.padding(16.dp)
     )
 }
